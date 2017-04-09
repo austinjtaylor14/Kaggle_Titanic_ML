6ef2fb8646a70c4a083ce8ee3d80071d7d2247bb
Austin <austinjtaylor14@gmail.com>
2017-04-09 12:14:54 -0400

    Initial commit

---
+++ titanic_model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  9 12:12:34 2017
+
+@author: Austin
+"""
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import roc_auc_score
+import pandas as pd
+
