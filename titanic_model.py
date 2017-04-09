08f34693adb375a802bc8bd8308cc0227d9c6b39
Austin <austinjtaylor14@gmail.com>
2017-04-09 12:37:52 -0400

    Additional data cleaning

--- titanic_model.py
+++ titanic_model.py
@@ -6,7 +6,19 @@ Created on Sun Apr  9 12:12:34 2017
 @author: Austin
 """
 
+# Imports
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import roc_auc_score
 import pandas as pd
 
+# Read in data
+df_training_data = pd.read_csv('./Kaggle_Titanic_ML/csv_files/train.csv')
+df_training_data.pop('Survived')
+
+# Since 'Age' has missing values, we need to fill in those values with the 
+# mean age of the entire ship
+df_training_data['Age'].fillna(df_training_data.Age.mean(), inplace = True)
+
+# Get non-object columns and subset original data
+numeric_cols = [index for index, dtype in df_training_data.dtypes.iteritems() if dtype != 'object']
+df_training_data[numeric_cols].head()
