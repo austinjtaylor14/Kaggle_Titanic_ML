64ead27d7943e0c57d721395b583d85830e71c46
Austin <austinjtaylor14@gmail.com>
2017-04-09 13:00:10 -0400

    General maintenance

--- titanic_model.py
+++ titanic_model.py
@@ -11,9 +11,10 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import roc_auc_score
 import pandas as pd
 
+## READING AND CLEANING DATA ##
 # Read in data
 df_training_data = pd.read_csv('./Kaggle_Titanic_ML/csv_files/train.csv')
-df_training_data.pop('Survived')
+df_training_survived = df_training_data.pop('Survived')
 
 # Since 'Age' has missing values, we need to fill in those values with the 
 # mean age of the entire ship
@@ -24,6 +25,10 @@ numeric_cols = [index for index, dtype in df_training_data.dtypes.iteritems() if
 df_training_data[numeric_cols].head()
 
 
-# Making our model
+## MAKING OUR MODEL ##
+
+# Setting up model parameters and fitting the numeric variables of our data 
+# to the 'Survived' column
 titanic_model = RandomForestRegressor(n_estimators=100, oob_score=True, random_state=42)
-titanic_model.fit(df_training_data[numeric_cols], df_training_data)
+titanic_model.fit(df_training_data[numeric_cols], df_training_survived)
+titanic_model.oob_score_
